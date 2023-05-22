c43135c61810025f31aa4f5bb5cc0e9947c03f31
kkoouuya0730 <kouya.0730@icloud.com>
2023-05-22 12:05:23 +0900

    :construction: Progress until 5/21

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, drop_database
+from demo.database import Base
+
+
+@pytest.fixture(scope="function")
+def SessionLocal():
+    TEST_SQLALCHEMY_DATABASE_URL = (
+        "postgresql://user:kaorunagisa09@localhost:5432/todo"
+    )
+    engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL)
+    # assert not database_exists(
+    #     TEST_SQLALCHEMY_DATABASE_URL
+    # ), "Test database already exists. Aborting tests."
+
+    Base.metadata.create_all(engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    yield SessionLocal
+
+    drop_database(TEST_SQLALCHEMY_DATABASE_URL)
---
+++ tests/routers/test_todos.py
@@ -0,0 +1,39 @@
+from demo import __version__
+from demo.main import app
+from fastapi.testclient import TestClient
+from demo.database import get_db
+
+def temp_db(f):
+    def func(SessionLocal, *args, **kwargs):
+        def override_get_db():
+            try:
+                db = SessionLocal()
+                yield db
+            finally:
+                db.close()
+
+        app.dependency_overrides[get_db] = override_get_db
+        f(*args, **kwargs)
+        app.dependency_overrides[get_db] = get_db
+    return func
+
+client = TestClient(app)
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+# # def test_list_todos():
+# #     response = client.get("/todos/")
+# #     assert response.status_code == 200
+
+
+# # def test_get_todo():
+# #     response = client.get("/{todo_id}/")
+# #     assert response.status_code == 200
+
+@temp_db
+def test_create_todo():
+    response = client.post("/todos", json={"title": "temp_db_test", "description": "temp_db_test"})
+    assert response.status_code == 200
